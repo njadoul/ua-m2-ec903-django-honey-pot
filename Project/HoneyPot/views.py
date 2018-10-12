e2f838febfad9c77576c6c594f5bfa41306fa124
freshloic <loicjeanchrles@gmail.com>
2018-10-12 07:53:39 +0200

    Ajout des urls et des vues de base

--- Project/HoneyPot/views.py
+++ Project/HoneyPot/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse('<h1>Page login</h1>')
+
+def contact(request):
+    return HttpResponse('<h1>Page contact</h1>')
+
+def conditions(request):
+    return HttpResponse('<h1>Page conditions</h1>')
+
+def donnees(request):
+    return HttpResponse('<h1>Page donnees</h1>')
+
+def mentions(request):
+    return HttpResponse('<h1>Page mentions</h1>')
