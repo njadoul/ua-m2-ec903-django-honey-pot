1b2ca327841ba947e722c43f1a7ae42a2ee2e84d
njadoul <nathanjadoul@live.fr>
2018-10-12 03:20:23 +0200

    Création de la vu du formulaire de contact

--- Project/HoneyPot/views.py
+++ Project/HoneyPot/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .forms import Contact
 
-# Create your views here.
+def contact(request):
+    form = Contact(request.POST or None)
+    if form.is_valide():
+        nom = form.cleaned_data['nom']
+        prénom = form.cleaned_data['prénom']
+        mail = form.cleaned_data['mail']
+        message = form.cleaned_data['message']
+
+    return render(request, 'HoneyPot/contact.html', locals())
