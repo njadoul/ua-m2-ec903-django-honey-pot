0f27849d8b779c470d639151a49e291b929f3c9c
njadoul <nathanjadoul@live.fr>
2018-10-13 22:12:37 +0200

    modif d'admin pour permettre l'afichage de la date

--- Project/HoneyPot/admin.py
+++ Project/HoneyPot/admin.py
@@ -3,10 +3,10 @@ from .models import Contact, Connex
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ['ip']
+    list_display = ['ip', 'date']
     ordering = ['date']
 
 @admin.register(Connex)
 class ConnexAdmin(admin.ModelAdmin):
-    list_display = ['ip']
+    list_display = ['ip', 'date']
     ordering = ['date']
