7e3cf58d5cd11074edcc33880374090ab73ab5be
njadoul <nathanjadoul@live.fr>
2018-10-13 14:21:51 +0200

    Modification du model et application des migrations

--- Project/HoneyPot/models.py
+++ Project/HoneyPot/models.py
@@ -1,12 +1,32 @@
 from django.db import models
+from django.utils import timezone
 
 class Profil(models.Model):
     login = models.TextField(max_length = 50)
     password = models.TextField(max_length = 50)
+    ip = models.TextField(max_length = 50)
+    user_agent = models.TextField(max_length = 50)
+    date = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ['date']
+        verbose_name = "Formulaire de Login"
+
+    def _str_(self):
+        return self.ip
+
+class Contact(models.Model):
     nom = models.TextField(max_length = 50)
     prénom = models.TextField(max_length = 50)
     mail = models.EmailField(max_length = 50)
     message = models.TextField(max_length = 50)
     ip = models.TextField(max_length = 50)
     user_agent = models.TextField(max_length = 50)
-    date = models.DateField()
+    date = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ['date']
+        verbose_name = "Formulaire de Contact"
+
+    def _str_(self):
+        return self.ip
--- Project/Project/urls.py
+++ Project/Project/urls.py
@@ -18,5 +18,4 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('contact/', views.contact, name = 'contact'),
 ]
