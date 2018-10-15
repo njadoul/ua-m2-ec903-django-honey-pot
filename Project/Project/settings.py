da7324f85edd55eef4666ba6e7c5cef77c7e8673
freshloic <loicjeanchrles@gmail.com>
2018-10-15 03:20:38 +0200

    Add static configuration

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -125,7 +125,8 @@ STATIC_URL = '/static/'
 
 # Add these new lines
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    # os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'HoneyPot/static'),
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
