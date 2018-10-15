de747b336908027b466bb657b5f31d4ba3aaa4dd
freshloic <loicjeanchrles@gmail.com>
2018-10-15 04:30:06 +0200

    Ajout de whitenoise pour la gestion des fichiers static en production

--- Project/Project/prod_settings.py
+++ Project/Project/prod_settings.py
@@ -5,3 +5,7 @@ DEBUG = False
 SECRET_KEY = '4(3aipi5unhc53c53si#_d8@*!f%gsr0l6ri6$+=xi@a=+5@b='
 
 ALLOWED_HOSTS = ['nathanetloic.herokuapp.com']
+
+MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage
