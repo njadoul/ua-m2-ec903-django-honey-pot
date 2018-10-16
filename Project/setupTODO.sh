53e888da90c0ad97ea4146dff3151a098a6f999a
JEAN-CHARLES Loïc ( freshloic) <loicjeancharles@gmail.com>
2018-10-16 01:42:49 -0300

    Update setupTODO.sh

--- Project/setupTODO.sh
+++ Project/setupTODO.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd Project
 python3 manage.py makemigrations
 python3 manage.py migrate
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'pass')" | python3 manage.py shell
