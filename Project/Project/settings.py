7fc3d7836f4758bf986fcb74e64bcee174bef1ce
JEAN-CHARLES Loïc ( freshloic) <loicjeancharles@gmail.com>
2018-10-16 18:56:43 -0300

    Réglage du bug django gulp

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -41,9 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-
-    # 'django_gulp',
-
+    'django_gulp',
     'django.contrib.staticfiles',
 ]
 
